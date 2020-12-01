6d96a8182c38935f43aae37ac1e7d1c7406124db
Omar Laptop <lappy@pop-os.localdomain>
2020-11-30 17:00:19 -0800

    createVCard signature

---
+++ vCardProcessing.py
@@ -0,0 +1,5 @@
+# External Libraries
+import vobject
+
+def createVCard(first_name: str = None, last_name : str = None,  email = None, email_type = None, tel = None, tel_type = None, address = None, address_type = None, title: str = None, company: str = None):
+    return ""
